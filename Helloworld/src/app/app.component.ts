caf2768effc2d14f90af52fffa6c8758158d1f6e
deepakchoursiya <chourasiadeepak2002@gmail.com>
2025-03-06 09:58:29 +0530

    Deepak Add. property data binding

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -3,12 +3,14 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Helloworld';
+  imgUrl = "https://i.pinimg.com/736x/17/a4/ab/17a4abbc769be0d348f9e52a6fb20232.jpg";  
   
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz";
