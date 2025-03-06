f4a6e8fdd4caa1fff9b3309238d6188795392376
deepakchoursiya <chourasiadeepak2002@gmail.com>
2025-03-06 10:03:18 +0530

    Deepak Add. event binding

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -6,13 +6,19 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']  // ✅ Fixed typo
 })
 export class AppComponent {
   title = 'Helloworld';
   imgUrl = "https://i.pinimg.com/736x/17/a4/ab/17a4abbc769be0d348f9e52a6fb20232.jpg";  
-  
+  url = "https://www.bridgelabz.com";
+
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz";
+  }
+
+  onClick($event: MouseEvent) {  // ✅ Added type
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
   }
 }
