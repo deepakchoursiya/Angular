0295f36ac0ab7ad4250c6e4e1ef199badcf4d12f
deepakchoursiya <chourasiadeepak2002@gmail.com>
2025-03-06 10:05:05 +0530

    Deepak Add. two way binding

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -1,23 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule], // Add FormsModule here
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']  // ✅ Fixed typo
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Helloworld';
   imgUrl = "https://i.pinimg.com/736x/17/a4/ab/17a4abbc769be0d348f9e52a6fb20232.jpg";  
   url = "https://www.bridgelabz.com";
+  userName: string = ""; // Initialized userName variable
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz";
   }
 
-  onClick($event: MouseEvent) {  // ✅ Added type
+  onClick($event: MouseEvent) {  
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
   }
