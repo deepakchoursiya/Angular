645b93a3a04fec55ce7c3bb3a57d4be876a0d508
deepakchoursiya <chourasiadeepak2002@gmail.com>
2025-03-06 10:07:11 +0530

    Deepak Add. input event binding

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent {
   imgUrl = "https://i.pinimg.com/736x/17/a4/ab/17a4abbc769be0d348f9e52a6fb20232.jpg";  
   url = "https://www.bridgelabz.com";
   userName: string = ""; // Initialized userName variable
+  nameError: string = "";
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz";
@@ -22,5 +23,11 @@ export class AppComponent {
   onClick($event: MouseEvent) {  
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
+  }
+
+  onInput() {  
+    console.log("Change Event Occurred!", this.userName);
+    const nameRegex = /^[A-Z]{1}[a-zA-Z\s]{2,}$/;
+    this.nameError = nameRegex.test(this.userName) ? "" : "Name is Incorrect!";
   }
 }
